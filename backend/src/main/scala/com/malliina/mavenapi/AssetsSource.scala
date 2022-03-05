29cf4f5650020845ee0ef2507656f87ffb77146e
Michael Skogberg <malliina123@gmail.com>
2022-03-05 15:05:18 +0200

    Fix address

--- backend/src/main/scala/com/malliina/mavenapi/AssetsSource.scala
+++ backend/src/main/scala/com/malliina/mavenapi/AssetsSource.scala
@@ -9,7 +9,7 @@ trait AssetsSource:
 
 object AssetsSource:
   def apply(isProd: Boolean): AssetsSource =
-    if isProd then CDNAssets(FullUrl.https("cdn-api.malliina.com", ""))
+    if isProd then CDNAssets(FullUrl.https("api-cdn.malliina.com", ""))
     else DirectAssets
 
 object DirectAssets extends AssetsSource:
