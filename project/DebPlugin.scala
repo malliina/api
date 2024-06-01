75df8e9e35d68a82f85d007c793ebb9dcbbde986
Michael Skogberg <michael.skogberg@futurice.com>
2024-06-01 22:01:31 +0300

    Deploy

--- project/DebPlugin.scala
+++ project/DebPlugin.scala
@@ -1,12 +1,18 @@
-import sbt.Keys.{name, publishArtifact, mappings, packageDoc}
-import sbt.{Compile, Setting, AutoPlugin}
+import sbt.Keys.{baseDirectory, name, publishArtifact, mappings, packageBin, packageDoc, streams}
+import sbt._
 import com.typesafe.sbt.packager.linux.LinuxPlugin.autoImport.{Linux, daemonUser}
+import com.typesafe.sbt.packager.debian.DebianPlugin.autoImport.Debian
 import com.typesafe.sbt.packager.archetypes.systemloader.SystemdPlugin
 import com.typesafe.sbt.packager.archetypes.JavaServerAppPackaging
 import com.typesafe.sbt.SbtNativePackager.autoImport.{packageName, executableScriptName, packageSummary, maintainer, packageDescription}
 
 object DebPlugin extends AutoPlugin {
   override def requires = JavaServerAppPackaging && SystemdPlugin
+
+  object autoImport {
+    val Deb = config("Deb")
+  }
+  import autoImport.Deb
 
   override def projectSettings: Seq[Setting[?]] = Seq(
     Linux / name := name.value,
@@ -17,6 +23,15 @@ object DebPlugin extends AutoPlugin {
     executableScriptName := (Linux / name).value,
     Compile / packageDoc / mappings := Nil,
     Compile / packageDoc / publishArtifact := false,
-    maintainer := "Michael Skogberg <malliina123@gmail.com>"
+    maintainer := "Michael Skogberg <malliina123@gmail.com>",
+    Deb / packageBin := {
+      val artifact = (Debian / packageBin).value
+      val destName = (Linux / name).value
+      val dest = baseDirectory.value / destName
+      IO.copyFile(artifact, dest)
+      streams.value.log.info(s"Copied '$artifact' to '$dest'.")
+      dest
+    },
+    Deb / packageBin := (Deb / packageBin).dependsOn(Debian / packageBin).value
   )
 }
