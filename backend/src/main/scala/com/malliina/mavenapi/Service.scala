27f9370009588bf07327ea652be514bb8816945a
Michael Skogberg <malliina123@gmail.com>
2025-02-28 16:17:01 +0200

    Deps

--- backend/src/main/scala/com/malliina/mavenapi/Service.scala
+++ backend/src/main/scala/com/malliina/mavenapi/Service.scala
@@ -14,11 +14,11 @@ import org.http4s.{HttpRoutes, Request, Response, Uri}
 object Service:
   val pong = "pong"
 
-  def default[F[_]: Async: Parallel](http: HttpClientF2[F]): Service[F] =
+  def default[F[_]: { Async, Parallel }](http: HttpClientF2[F]): Service[F] =
     val db = MyDatabase[F]
     Service[F](MavenCentralClient[F](http), db)
 
-class Service[F[_]: Async: Parallel](maven: MavenCentralClient[F], data: MyDatabase[F])
+class Service[F[_]: { Async, Parallel }](maven: MavenCentralClient[F], data: MyDatabase[F])
   extends AppImplicits[F]
   with FormDecoders[F]:
   private val pages = Pages.default()
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -8,6 +8,6 @@ Seq(
   "com.malliina" % "sbt-filetree" % utilsVersion,
   "org.scalameta" % "sbt-scalafmt" % "2.5.4",
   "org.portable-scala" % "sbt-scalajs-crossproject" % "1.3.2",
-  "com.eed3si9n" % "sbt-assembly" % "2.3.0",
-  "com.github.sbt" % "sbt-native-packager" % "1.11.0"
+  "com.eed3si9n" % "sbt-assembly" % "2.3.1",
+  "com.github.sbt" % "sbt-native-packager" % "1.11.1"
 ) map addSbtPlugin
